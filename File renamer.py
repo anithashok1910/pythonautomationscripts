05c55a10b127f553c95dd8b3879c7ec4e574ed74
Anith Ashok <anith.ashok1910@gmail.com>
2016-02-15 23:22:37 +0530

    File renamer.py is a python script which is used to move the files from one location to another. The name of files that are to be moved from one location to another is specified in a text file called 'Unsupported Files.txt' and the files that are meant to be moved are unsupported pdf files from

---
+++ File renamer.py
@@ -0,0 +1,27 @@
+"""
+
+File renamer.py is a python script which is used to move the files from one location to another.
+The name of files that are to be moved from one location to another is specified in a text
+file called 'Unsupported Files.txt' and the files that are meant to be moved are unsupported
+pdf files from the yesterday's work.
+The program should be executed once only after the successful execution as it will give error
+if the files and folders already exists or are moved to the location.
+
+
+INCOMPLETE FILE
+WORK IS YET TO BE DONE FULLY
+"""
+
+import os
+import shutil
+
+fileobj = open('Unsupported Files.txt','r')
+contents = fileobj.read()
+
+contentlist = contents.split('\n')
+
+os.chdir('H:\\backup\\pdf')
+
+for file in contentlist:
+    shutil.rename(file,'H:\\backup\\Unsupported pdf\\')
+fileobj.close()
