3250ef16916eb9ede54753d1261f8aa4ea273b74
Anith Ashok <anith.ashok1910@gmail.com>
2016-02-17 23:32:06 +0530

    Audio fixer.py is a python script file which uses the meta data to rename the audio files and based on their metadata stores them in their individual directories created as per their album names.

---
+++ Audio fixer.py
@@ -0,0 +1,20 @@
+"""
+Audio fixer.py is a python script file which uses the meta data to rename the audio files and based on their metadata
+stores them in their individual directories created as per their album names.
+"""
+
+import os
+import eyed3
+import shutil
+
+def nomenclature():
+    """
+
+    :return:
+    """
+
+def file_mover():
+    """
+
+    :return:
+    """
